cf6799dba6e7fcf39826f589c08ae2d0640bf998
Casian Blanaru <cab@tpwd.de>
2025-04-15 18:21:06 +0200

    Refaktoriere Logger-Klasse zu einem Objekt mit Methoden für verschiedene Protokollierungslevel, um die Lesbarkeit und Wartbarkeit des Codes zu verbessern.

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,30 +1,13 @@
 const chalk = require('chalk');
 const logSymbols = require('log-symbols');
 
-class Logger {
-  static info(message) {
-    console.log(chalk.blue(logSymbols.info), chalk.blue(message));
-  }
+const logger = {
+  info: (message) => console.log(chalk.blue(logSymbols.info), chalk.blue(message)),
+  success: (message) => console.log(chalk.green(logSymbols.success), chalk.green(message)),
+  warning: (message) => console.log(chalk.yellow(logSymbols.warning), chalk.yellow(message)),
+  error: (message) => console.log(chalk.red(logSymbols.error), chalk.red(message)),
+  waiting: (message) => console.log(chalk.cyan('⏳'), chalk.cyan(message)),
+  screenshot: (message) => console.log(chalk.magenta('📸'), chalk.magenta(message))
+};
 
-  static success(message) {
-    console.log(chalk.green(logSymbols.success), chalk.green(message));
-  }
-
-  static warning(message) {
-    console.log(chalk.yellow(logSymbols.warning), chalk.yellow(message));
-  }
-
-  static error(message) {
-    console.log(chalk.red(logSymbols.error), chalk.red(message));
-  }
-
-  static waiting(message) {
-    console.log(chalk.cyan('⏳'), chalk.cyan(message));
-  }
-
-  static screenshot(message) {
-    console.log(chalk.magenta('📸'), chalk.magenta(message));
-  }
-}
-
-module.exports = Logger; 
+module.exports = logger; 
